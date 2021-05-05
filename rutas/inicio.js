a25f8940f348e46904b8dd494f0b4014dbad4129
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-05-05 16:10:27 -0600

    modificando backend para base de datos

---
+++ rutas/inicio.js
@@ -0,0 +1,64 @@
+const { Router } =  require('express');
+const router = Router();
+
+
+inicio = router.post("/login", (req,res)=>{
+    let data= req.body;
+    console.log(data)
+    Usuario.find({ usuario: data.Usuario, contrasena: data.Contrasena}, 
+        function (err, usuario)
+        {
+            if(err || usuario.length === 0)
+            {
+                console.log(err)
+                res.json({
+                    estado: "FALLIDO",
+                    mensaje: "Credenciales incorrectas"
+                })
+                return
+            }
+            console.log(usuario)
+            res.json({
+                estado: "EXITOSO",
+                mensaje: "Bienvenido"    
+            })         
+        });
+})
+
+registro = router.post("/register", (req,res)=>{
+    let data= req.body;
+    
+    Usuario.find({ usuario: data.Usuario}, 
+        function (err, usuarios)
+        {
+            if(err || usuarios.length !== 0)
+            {
+                console.log(err)
+                res.json({
+                    estado: "FALLIDO",
+                    mensaje:"Usuario ya existe"
+                })
+                return
+            }
+            const usuario = new Usuario({ usuario: data.Usuario, contrasena: data.Contrasena});      
+            usuario.save(function (err, usuario) {
+                if(err)
+                {
+                    console.log(err)
+                    res.json({
+                        estado: "FALLIDO",
+                        mensaje:"Error al crear el usuario"
+                    })
+                    return
+                }
+                console.log(usuario)
+                res.json({
+                    estado: "EXITOSO",
+                    mensaje:"Usuario creado"
+                })         
+              });
+        });
+
+});
+
+module.exports = router;
