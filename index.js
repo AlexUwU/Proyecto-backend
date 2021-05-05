9f2cd2273ee853b351a205c96019933a9112b25a
bkevin1052 <47799833+AlexUwU@users.noreply.github.com>
2021-05-05 17:05:52 -0600

    pruebas sin cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const dotenv = require("dotenv")
 const bodyparser = require("body-parser")
-var cors= require("cors")
+const cors = require("cors")
 const mongoose = require("mongoose")
 
 dotenv.config()
@@ -11,7 +11,7 @@ require('./basededatos');
 
 app.set('port', 3000);
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 
 app.get('/', (req,res) => res.send('Petroll - Backend'));
 
